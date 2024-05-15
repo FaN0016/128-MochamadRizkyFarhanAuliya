c4418a0225590788fcc4ec57f6d8049dbcfca2d3
FaN0016 <16farhanauliya@gmail.com>
2024-05-15 18:53:55 +0700

    Add something to homepage.php

--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -8,5 +8,12 @@
 <body>
     <h2>This is just test first</h2>
     <p>I hope I have some time to do this again</p>
+    <h3>TODO LIST</h3>
+    <ul>
+        <li>Rapihin dataset</li>
+        <li>Buat basic Design</li>
+        <li>Cari waktu buat beresin</li>
+        <li>????</li>
+    </ul>
 </body>
 </html>
