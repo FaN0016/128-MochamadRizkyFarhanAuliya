6578a7bd65d38ff2675e5de443634262a5c85a58
FaN0016 <16farhanauliya@gmail.com>
2024-05-15 18:42:26 +0700

    add homepage.php and adding route to homepage.php

---
+++ resources/views/homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoL Pro Stats</title>
+</head>
+<body>
+    <h2>This is just test first</h2>
+    <p>I hope I have some time to do this again</p>
+</body>
+</html>
